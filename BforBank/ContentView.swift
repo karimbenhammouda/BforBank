be9368d8be53f5c94934c8af0ebc3f19d2060659
karimbenhammouda <karim.hammouda.bhk@gmail.com>
2024-10-23 14:53:01 +0200

    create template project

---
+++ BforBank/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  BforBank
+//
+//  Created by karim on 23/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+        }
+    }
+}
